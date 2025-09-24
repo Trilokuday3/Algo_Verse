24af7cc0efc56380403f2aa6e94286b10774deb8
Chebrolu Trilokesh Venkata Uday <trilokeshvenkatauday@gmail.com>
2025-09-24 18:28:26 +0530

    Delete Bin directory

--- Bin/test.py
+++
@@ -1,9 +0,0 @@
-print("--- Starting Trading Script ---")
-
-stock_name = 'NIFTY'
-ltp = tsl.get_ltp_data(names=[stock_name])[stock_name]
-print(f"LTP of {stock_name}: {ltp}")
-
-print("\n--- Script Finished ---")
-
-print("Hi")
